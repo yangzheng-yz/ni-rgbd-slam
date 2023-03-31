63187e9bd02e2144d4a56261ab3f541eda6e0b6c
yangzheng-yz <yangzhengtj@outlook.com>
2023-03-31 14:26:10 +0800

    update ofkt0 result

--- evo.sh
+++ evo.sh
@@ -1,6 +1,6 @@
 evo_traj tum \
-/home/zheng/datasets/public_dataset/ICL-NUIM/of_kt1/groundtruth.txt \
---ref /home/zheng/projects/ni_slam_ws/src/ni_slam/bag/icl_ofkt1_2023_Mar_31_12_04_45.txt \
+/home/zheng/datasets/public_dataset/ICL-NUIM/of_kt0/groundtruth.txt \
+--ref /home/zheng/projects/ni_slam_ws/src/ni_slam/bag/icl_ofkt0_2023_Mar_31_13_21_43.txt \
 -p -as
 # --pose_relation angle_deg -p 
 # evo_rpe tum /home/zheng/datasets/public_dataset/ICL-NUIM/of_kt1/groundtruth.txt /home/zheng/projects/rotation_estimation/nor2rot/results/test_ofkt1__improve1_3cell10J10.txt --pose_relation angle_deg --delta 1 --delta_unit f -p -va # -va --plot --plot_mode xyz -r angle_deg
